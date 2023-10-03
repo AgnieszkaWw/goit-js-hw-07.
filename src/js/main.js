4a1f150532f648524c486350af6a2fdd7a924bce
AgnieszkaWw <agnieszkawie82@onet.pl>
2023-10-03 21:19:25 +0200

    First commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 const categoriesList = document.querySelector("#categories");
 const categoryItems = categoriesList.querySelectorAll(".item");
 const categoryCount = categoryItems.length;
-console.log(`Liczba kategorii: ${categoryCount}`);
+console.log('Liczba kategorii: ' , categoryCount);
 categoryItems.forEach(categoryItem => {
 	const categoryTitle = categoryItem.querySelector("h2").textContent;
 	const categoryElements = categoryItem.querySelectorAll("ul li");
 	const categoryElementCount = categoryElements.length;
-	console.log(`${categoryTitle}: ${categoryElementCount}`);
+	console.log('categoryTitle: ');
+	console.log('categoryElementCount: ');
 });
